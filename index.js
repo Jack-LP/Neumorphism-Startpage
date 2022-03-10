640a8a2bc7518daab9f6df26b6d2790bcab5b968
Jack Paget <jackpaget1@gmail.com>
2022-03-10 11:45:55 +0000

    Added dark mode switch

---
+++ index.js
@@ -0,0 +1,16 @@
+localStorage.getItem('theme');
+
+const toggle = document.getElementById("toggle");
+const theme = window.localStorage.getItem("theme");
+
+if (theme === "dark") document.body.classList.add("dark")
+;
+
+toggle.addEventListener("click", () => {
+   document.body.classList.toggle("dark");
+   if (theme === "dark") {
+     window.localStorage.setItem("theme", "light");
+   } else window.localStorage.setItem("theme", "dark");
+});
+
+
